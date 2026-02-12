215d9ee8335c694ad13dd99cc3161b811983b6ce
Öznur Karahasan <154363611+oznurkarahasan@users.noreply.github.com>
2026-02-12 18:15:57 +0300

    Revert "2 real time speech to text"

--- backend/main.py
+++ backend/main.py
@@ -18,7 +18,7 @@ from app.core.exceptions import (
     ResourceNotFoundError,
     ValidationError
 )
-from app.api.v1 import auth, presentations, chat, transcribe, live
+from app.api.v1 import auth, presentations, chat
 
 # Lifespan event to create tables and extensions
 @asynccontextmanager
@@ -133,8 +133,6 @@ app.mount("/uploaded_files", StaticFiles(directory="uploaded_files"), name="uplo
 app.include_router(auth.router, prefix=settings.API_V1_STR + "/auth", tags=["Authentication"]) # Authentication routes modules seperated
 app.include_router(presentations.router, prefix=settings.API_V1_STR + "/presentations", tags=["Presentations"])
 app.include_router(chat.router, prefix=settings.API_V1_STR + "/chat", tags=["Chat"])
-app.include_router(transcribe.router, prefix=settings.API_V1_STR + "/tools", tags=["STT"])
-app.include_router(live.router, prefix=settings.API_V1_STR + "/live", tags=["Live"])
 @app.get("/")
 async def root():
     return {"message": "AI Presentation Assistant API is running successfully."}
